57aa3835a8e128b795ff28983c9067be3e365a86
senjansen <senjansen.sj@gmail.com>
2019-07-06 15:54:54 +0700

    upload exercise 8

---
+++ exercise-8.js
@@ -0,0 +1,17 @@
+function pasanganTerbesar(num) {
+    // you can only write your code here!
+    var numString = String(num).split('');
+    var max = Math.max.apply(null, numString);
+    max = String(max);
+    var index = numString.indexOf(max) + 1;
+    var next = numString[index];
+    var res = max + next;
+    return res;
+}
+
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
